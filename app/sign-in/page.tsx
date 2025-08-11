c5d8f358dcf7cc617dab69339a42876698241cc7
Juicedream <judexfrayo@gmail.com>
2025-08-11 12:13:15 +0100

    Initial Commit

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,6 @@
+const page = () => {
+  return (
+    <div>Sign in</div>
+  )
+}
+export default page
---
+++ app/subscription/page.tsx
@@ -0,0 +1,6 @@
+const subscription = () => {
+  return (
+    <div>subscription</div>
+  )
+}
+export default subscription
